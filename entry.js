487223142f30d6d2ffcc30b066132d5c7c84b891
Eleazar Corpuz <eleazarcorpuz@gmail.com>
2016-08-05 20:30:37 -0700

    transfered scripts fro index to entry.js

--- entry.js
+++ entry.js
@@ -1,5 +1,10 @@
 require("./css/style.css");
 
+// $script("https://cdnjs.cloudflare.com/ajax/libs/tether/1.3.3/js/tether.min.js");
+// $script("https://cdnjs.cloudflare.com/ajax/libs/jquery/2.0.0/jquery.min.js");
+
+require('./node_modules/angular/angular.min.js');
+require('./public/js/controller/main.js');
 
 var otherComponent = require("./js/content.js");
 
